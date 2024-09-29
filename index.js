c2e0a6a349ac1f1b053a9dec7ef947063f754391
chris <Mileschris466@gmail.com>
2024-09-29 03:01:20 +0300

    functions commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+
+function  saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+    }
+    
+
+consolelog(saturdayFun);
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+
+    function mondayWork(activity="work from home."){
+
+        retturn `This Monday, I will ${activity}.`;
+    }}
+
+ console.log(mondayWork);   
+
+ function wrapAdjective(feeling="special"){
+
+    return `You are ..! ${feeling}`
+    
+    }
+
+    function wrapAdjective(tag="*"){
+        return function (feeling="special"){
+        return `You are ${tag}${feeling}${tag}!`;
+    }
+}
+        
+        console.log(wrapAdjective("*"));
+    
